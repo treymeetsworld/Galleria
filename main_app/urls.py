0f8b314b61ab91bc9d4e966f62605250ce6dfd5c
Trevor Mwangi <trevorzac16@yahoo.com>
2021-12-29 07:30:03 -0500

    add login/signup

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.Home.as_view(), name='home'),
+  path('accounts/signup/', views.signup, name='signup'),
   
 ]
