d8f3f3b592bac9f24df08ba50907e443d9fb156d
Trevor Mwangi <trevorzac16@yahoo.com>
2022-01-07 11:23:46 -0500

    css update

--- main_app/views.py
+++ main_app/views.py
@@ -27,7 +27,7 @@ def signup(request):
     if form.is_valid():
       user = form.save()
       login(request, user)
-      return redirect('cats_index')
+      return redirect('galleries_index')
     else:
       error_message = 'Invalid sign up - try again'
   form = UserCreationForm()
